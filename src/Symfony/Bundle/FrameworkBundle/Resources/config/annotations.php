6344b7d02d2dc50b308bf0d07f368f4d37755ae9
Alexis Lefebvre <alexislefebvre+github@gmail.com>
2023-01-08 00:08:15 +0100

    remove compatibility layer for doctrine/annotations < 1.6

--- src/Symfony/Bundle/FrameworkBundle/Resources/config/annotations.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/config/annotations.php
@@ -12,7 +12,6 @@
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Doctrine\Common\Annotations\AnnotationReader;
-use Doctrine\Common\Annotations\AnnotationRegistry;
 use Doctrine\Common\Annotations\PsrCachedReader;
 use Doctrine\Common\Annotations\Reader;
 use Symfony\Bundle\FrameworkBundle\CacheWarmer\AnnotationsCacheWarmer;
@@ -25,11 +24,7 @@ return static function (ContainerConfigurator $container) {
         ->set('annotations.reader', AnnotationReader::class)
             ->call('addGlobalIgnoredName', [
                 'required',
-                service('annotations.dummy_registry')->ignoreOnInvalid(), // dummy arg to register class_exists as annotation loader only when required
             ])
-
-        ->set('annotations.dummy_registry', AnnotationRegistry::class)
-            ->call('registerUniqueLoader', ['class_exists'])
 
         ->set('annotations.cached_reader', PsrCachedReader::class)
             ->args([
